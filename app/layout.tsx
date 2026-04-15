b61da6f811f43119bb0d8068a27744ebd810549b
Luiz <luizhabluetzel@hotmail.ch>
2026-04-15 12:04:37 +0200

    Fix antd message context warning

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,45 +27,45 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        <ConfigProvider
-          theme={{
-            algorithm: theme.defaultAlgorithm,
-            token: {
-              // general theme options are set in token, meaning all primary elements (button, menu, ...) will have this color
-              colorPrimary: "#2f74b5", // selected input field border will have this color as well
-              borderRadius: 10,
-              colorText: "#2b2f35",
-              fontSize: 16,
+        <AntdRegistry>
+          <ConfigProvider
+            theme={{
+              algorithm: theme.defaultAlgorithm,
+              token: {
+                // general theme options are set in token, meaning all primary elements (button, menu, ...) will have this color
+                colorPrimary: "#2f74b5", // selected input field border will have this color as well
+                borderRadius: 10,
+                colorText: "#2b2f35",
+                fontSize: 16,
 
-              // Alias Token
-              colorBgContainer: "#f4f8fd",
-            },
-            // if a component type needs special styling, setting here will override default options set in token
-            components: {
-              Button: {
-                colorPrimary: "#2f74b5", // this will color all buttons in the new shared blue theme
-                algorithm: true, // enable algorithm (redundant with line 33 but here for demo purposes)
-                controlHeight: 42,
+                // Alias Token
+                colorBgContainer: "#f4f8fd",
               },
-              Input: {
-                colorBorder: "#b2c6db",
-                colorTextPlaceholder: "#6f86a0",
-                algorithm: false, // disable algorithm (line 32)
+              // if a component type needs special styling, setting here will override default options set in token
+              components: {
+                Button: {
+                  colorPrimary: "#2f74b5", // this will color all buttons in the new shared blue theme
+                  algorithm: true, // enable algorithm (redundant with line 33 but here for demo purposes)
+                  controlHeight: 42,
+                },
+                Input: {
+                  colorBorder: "#b2c6db",
+                  colorTextPlaceholder: "#6f86a0",
+                  algorithm: false, // disable algorithm (line 32)
+                },
+                Form: {
+                  labelColor: "#365777",
+                  algorithm: theme.defaultAlgorithm, // specify a specifc algorithm instead of true/false
+                },
+                Card: {},
               },
-              Form: {
-                labelColor: "#365777",
-                algorithm: theme.defaultAlgorithm, // specify a specifc algorithm instead of true/false
-              },
-              Card: {},
-            },
-          }}
-        >
-          <AntdRegistry>
+            }}
+          >
             <AntdApp>
               <div className="app-shell">{children}</div>
             </AntdApp>
-          </AntdRegistry>
-        </ConfigProvider>
+          </ConfigProvider>
+        </AntdRegistry>
       </body>
     </html>
   );
